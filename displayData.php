935fd29194e78eaa09a78944fc30ebe3f41b2ffa
chairunnisa982005 <chairunnisa982005@gmail.com>
2024-11-08 15:59:45 +0700

    Create displayData.php

---
+++ displayData.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Display Data</title>
+</head>
+<body>
+    <h1>Data yang Dikirim</h1>
+    <p>Email: <span id="displayEmail"></span></p>
+    <p>Password: <span id="displayPassword"></span></p>
+
+    <script>
+      // Mengambil data dari localStorage
+      document.getElementById('displayEmail').innerText = localStorage.getItem('email');
+      document.getElementById('displayPassword').innerText = localStorage.getItem('password');
+    </script>
+</body>
+</html>
